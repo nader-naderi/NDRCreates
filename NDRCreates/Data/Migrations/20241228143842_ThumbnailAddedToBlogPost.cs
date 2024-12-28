3f4c2ed966c6a30f321dc6c9c6e1106bf4885056
Nader Naderi <2200stellaris@gmail.com>
2024-12-28 18:39:41 +0330

    textEditor Added to the Blog Post Editor.

---
+++ NDRCreates/Data/Migrations/20241228143842_ThumbnailAddedToBlogPost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NDRCreates.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThumbnailAddedToBlogPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailPath",
+                table: "BlogPosts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailPath",
+                table: "BlogPosts");
+        }
+    }
+}
